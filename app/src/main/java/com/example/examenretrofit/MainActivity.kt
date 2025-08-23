59df7da364b97bd5dcb69900fd378455035f1dfa
Alejandro <77583757+betanS@users.noreply.github.com>
2025-08-23 23:25:03 +0100

    Finally, the app connects to the api, prints on the console all the pokemons when clicking the button. Also added a LazyColumn that on click updates and shows all the pokemons in specific cards.

--- app/src/main/java/com/example/examenretrofit/MainActivity.kt
+++ app/src/main/java/com/example/examenretrofit/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
@@ -43,8 +44,9 @@ class MainActivity : ComponentActivity() {
 fun PokemonScreen(onFetch: ((List<Pokemon>) -> Unit) -> Unit) {
     var pokemons by remember { mutableStateOf<List<Pokemon>>(emptyList()) }
 
-    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
-        Button(onClick = {
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+        Button( modifier = Modifier.height(80.dp).width(250.dp).padding(top = 50.dp), onClick = {
             onFetch { result ->
                 pokemons = result
                 // 👉 Logcat: imprime la lista completa
@@ -54,6 +56,32 @@ fun PokemonScreen(onFetch: ((List<Pokemon>) -> Unit) -> Unit) {
             }
         }) {
             Text("Cargar Pokemons")
+        }
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(pokemons) { pokemon ->
+                PokemonCard(pokemon)
+            }
+        }
+    }
+
+
+}
+
+
+
+@Composable
+fun PokemonCard(pokemon: Pokemon) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = "Nombre: ${pokemon.name}", style = MaterialTheme.typography.titleMedium)
+            Text(text = "Tipo: ${pokemon.type}", style = MaterialTheme.typography.bodyMedium)
+            Text(text = "Peso: ${pokemon.weight} kg", style = MaterialTheme.typography.bodySmall)
+            Text(text = "Altura: ${pokemon.height} cm", style = MaterialTheme.typography.bodySmall)
         }
     }
 }
